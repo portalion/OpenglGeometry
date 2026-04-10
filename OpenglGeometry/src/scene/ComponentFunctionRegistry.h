85e5b2ec59878e548ae468b9f828ba441c932227
Portalion <77tobik77@gmail.com>
2026-04-10 16:32:25 +0200

    change template arguments along bind

--- OpenglGeometry/src/scene/ComponentFunctionRegistry.h
+++ OpenglGeometry/src/scene/ComponentFunctionRegistry.h
@@ -2,28 +2,47 @@
 #include "Entity.h"
 #include <unordered_map>
 
-using ComponentAction = std::function<void(Entity)>;
-
+template<typename... Args>
 class ComponentFunctionRegistry
 {
+	using ComponentAction = std::function<void(Entity, Args...)>;
 private:
 	std::unordered_map<entt::id_type, ComponentAction> m_ComponentToFunctionStorage;
-
-protected:
+	
 	template<typename T>
-	void RegisterFunction(ComponentAction function)
-	{
-		m_ComponentToFunctionStorage[entt::type_hash<T>::value()] = function;
-	}
-
+	inline void RegisterFunction(ComponentAction function);
+protected:
+	template<typename TComponent, typename TClass>
+	inline void Bind(void(TClass::* memberFunc)(Entity, Args...));
 public:
-	inline void PerformFunctions(Entity entity) 
-	{
-		for (const auto& component : entity.GetComponentTypes())
-		{
-			if (!m_ComponentToFunctionStorage.contains(component)) continue;
-
-			m_ComponentToFunctionStorage[component](entity);
-		}
-	}
+	inline void PerformFunctions(Entity entity, Args... args);
 };
+
+
+template<typename ...Args>
+template<typename T>
+inline void ComponentFunctionRegistry<Args...>::RegisterFunction(ComponentAction function)
+{
+	m_ComponentToFunctionStorage[entt::type_hash<T>::value()] = function;
+}
+
+template<typename ...Args>
+template<typename TComponent, typename TClass>
+inline void ComponentFunctionRegistry<Args...>::Bind(void(TClass::* memberFunc)(Entity, Args...))
+{
+	RegisterFunction<TComponent>([this, memberFunc](Entity e, Args... args)
+		{
+			(static_cast<TClass*>(this)->*memberFunc)(e, std::forward<Args>(args)...);
+		});
+}
+
+template<typename ...Args>
+inline void ComponentFunctionRegistry<Args...>::PerformFunctions(Entity entity, Args ...args)
+{
+	for (const auto& component : entity.GetComponentTypes())
+	{
+		if (!m_ComponentToFunctionStorage.contains(component)) continue;
+
+		m_ComponentToFunctionStorage[component](entity, args...);
+	}
+}
--- OpenglGeometry/src/systems/gui/ShapeInspectorSystem.h
+++ OpenglGeometry/src/systems/gui/ShapeInspectorSystem.h
@@ -3,7 +3,7 @@
 #include <scene/ComponentFunctionRegistry.h>
 #include <interfaces/ISystem.h>
 
-class ShapeInspectorSystem : public ComponentFunctionRegistry, public ISystem
+class ShapeInspectorSystem : public ComponentFunctionRegistry<>, public ISystem
 {
 private:
 	Ref<Scene> m_Scene;
@@ -13,21 +13,8 @@ private:
 	void LineInspect(Entity entity);
 	void VirtualInspect(Entity entity);
 
-protected:
-	template<typename TComponent, typename TClass>
-	inline void Bind(void(TClass::* memberFunc)(Entity));
-
 public:
 	ShapeInspectorSystem(Ref<Scene> scene);
 
 	void Process() override;
 };
-
-template<typename TComponent, typename TClass>
-inline void ShapeInspectorSystem::Bind(void(TClass::* memberFunc)(Entity))
-{
-	RegisterFunction<TComponent>([this, memberFunc](Entity e)
-		{
-			(static_cast<TClass*>(this)->*memberFunc)(e);
-		});
-}
